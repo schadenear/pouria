3a1003108444c9b565e789d48376402455be7841
schadenear <94694825+schadenear@users.noreply.github.com>
2023-07-31 21:55:38 +0000

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x0f659383a20a51ca19e26fda13c27c220e2e8218";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
